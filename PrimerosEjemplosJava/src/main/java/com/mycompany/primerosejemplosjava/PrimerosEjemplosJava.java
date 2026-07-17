141189d1ea2c5bc02d6320769ff1fe0b353469c5
Gustavo Méndez <gmendez@probyteg.com>
2026-07-17 00:03:29 -0600

    Se muestran mensajes en pantalla

--- PrimerosEjemplosJava/src/main/java/com/mycompany/primerosejemplosjava/PrimerosEjemplosJava.java
+++ PrimerosEjemplosJava/src/main/java/com/mycompany/primerosejemplosjava/PrimerosEjemplosJava.java
@@ -1,16 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.primerosejemplosjava;
 
-/**
- *
- * @author gmendez
- */
+import java.util.Scanner;
+
+
+
 public class PrimerosEjemplosJava {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int nNumero = 0;
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Ingrese primer numero: ");
+        nNumero = in.nextInt();
     }
 }
