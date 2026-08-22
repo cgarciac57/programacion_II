f545aeea6f57f1532b65fdc4a30ab6b4af32e45b
Gustavo Méndez <gmendez@probyteg.com>
2026-08-22 17:28:56 -0600

    Se agrega clase Factura y los métodos CrearFactura y AnularFactura.

--- SistemaFacturacionSeccionA/src/main/java/Clases/Factura.java
+++ SistemaFacturacionSeccionA/src/main/java/Clases/Factura.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Clases;
 
-/**
- *
- * @author gmendez
- */
+import java.util.ArrayList;
+
 public class Factura {
+    
+    int IdFactura;
+    Cliente Clientes;
+    String Fecha;
+    ArrayList<DetalleFactura> DetalleFactura;
+    boolean Anulada;
+
+    public Factura(int IdFactura, Cliente Clientes, String Fecha) {
+        this.IdFactura = IdFactura;
+        this.Clientes = Clientes;
+        this.Fecha = Fecha;
+        this.DetalleFactura = new ArrayList<>();
+        this.Anulada = false; //Será cuándo mi factura está activa.
+    }
+    
+    //Crear Detalle Factura
+    public void CrearFacturaDetalle(DetalleFactura detalle){
+     //Agregar el detalle
+     DetalleFactura.add(detalle);
+    }
+    
+    //Anular Factura
+    public void AnularFactura(){
+        
+        if(this.Anulada = false){
+         this.Anulada = true;
+            System.out.println("La Factura No. " + this.IdFactura + " fue anulada");
+        }
+        
+    }
     
 }
